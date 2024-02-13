9db8214000a4f53deae78a7961c7388906ffce0e
Szymon Wojciechowski <kierej@gmail.com>
2024-02-13 19:48:24 +0100

    added notion-render class to the div with html injected

--- src/app/(pages)/package-300download/page.tsx
+++ src/app/(pages)/package-300download/page.tsx
@@ -1,22 +1,35 @@
+import { NotionRenderer } from "@notion-render/client";
 import ThemeToggle from "@/components/blocks/theme-toggle";
 import { Button } from "@/components/ui/button";
+import { getBlocks, notionClient } from "@/lib/notion";
 
-export default function Package300Download() {
+export default async function Package300Download() {
+  const notionRenderer = new NotionRenderer({
+    client: notionClient,
+  });
+
+  const notionBlock = (await getBlocks()) as any;
+
+  const html = await notionRenderer.render(...notionBlock);
+
   return (
-    <div>
+    <div className="p-20">
       <ThemeToggle />
-      <h1>
+      <Button variant="destructive">Add Button</Button>
+
+      <h1 className="text-4xl">
         <a href="https://github.com/kerwanp/notion-render">
           @notion-render/client
         </a>
       </h1>
-      <h2>
+      <h2 className="text-2xl underline p-2">
         <a href="https://notion-render-docs.vercel.app/guides/styling">
-          Dokumentcja
+          Dokumentcja link
         </a>
       </h2>
-      Package-300 download
-      <Button variant="destructive">Add Button</Button>
+      <p className="p-4">Package-300 download</p>
+      <div className="w-full h-16"></div>
+      <div className="notion-render" dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
 }
